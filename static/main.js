6cd847d094df3871cedb06fd9aba03c59aff42ce
chiconasa3000 <csucavelando@gmail.com>
2020-11-03 14:03:54 -0500

    acomodarArchivos

---
+++ static/main.js
@@ -0,0 +1,36 @@
+<script>
+	var canvas = document.querySelector('canvas')
+	var ctx = canvas.getContext('2d')
+	ctx.lineWidth = {{lw}}
+	var button = document.querySelector('button')
+	var mouse = {x: 0, y: 0}
+	
+	canvas.addEventListener('mousemove', function(e) {
+		mouse.x = e.pageX - this.offsetLeft
+		mouse.y = e.pageY - this.offsetTop
+	})
+	
+	canvas.onmousedown = ()=>{
+		ctx.beginPath()
+		ctx.moveTo(mouse.x, mouse.y)
+		canvas.addEventListener('mousemove', onPaint)
+	}
+	
+	canvas.onmouseup = ()=>{
+		canvas.removeEventListener('mousemove', onPaint)
+	}
+
+	var onPaint = ()=>{
+		ctx.lineTo(mouse.x, mouse.y)
+		ctx.stroke()
+	}
+	
+	var data = new Promise(resolve=>{
+		button.onclick = ()=>{
+			resolve(canvas.toDataURL('image/png'))
+			document.getElementById('canvasimg').value = canvas.toDataURL();
+		}
+	})
+
+</script>	
+
